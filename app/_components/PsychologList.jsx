756ccf8ba9b5cc829971d1573c7be6c4e5bfe348
Dhea Syifa Khoirunnisa <dheasyifa.khoirunnisa@spentera.com>
2024-03-07 11:53:31 +0700

    Updated

--- app/_components/PsychologList.jsx
+++ app/_components/PsychologList.jsx
@@ -22,7 +22,7 @@ const PsychologList = () => {
 
         <div className='mb-10 px-8'>
             <h2 className='font-bold text-xl'>
-                </h2>
+            </h2>
     
             <div className='grid grid-cols-2 
             sm:grid-cols-2 md:grid-cols-3
@@ -43,10 +43,10 @@ const PsychologList = () => {
                          />
                          <div className='mt-3 items-baseline flex flex-col gap-1'>
                              <h2 className='text-[10px] bg-blue-100 p-1 rounded-full
-                             px-2 text-primary'>Clinic</h2>
+                             px-2 text-primary'>{item.category}</h2>
                              <h2 className='font-bold'>{item.name}</h2>
                              <h2 className='text-primary text-sm'>{item.university}</h2>
-                             <h2 className='text-gray-500 text-sm'>{''}</h2>
+                             <h2 className='text-gray-500 text-sm'>{item.experience}</h2>
                              <Link href='/' className='w-full'>
                              <h2 className='p-2 px-3 border-[1px] border-primary
                              text-primary rounded-full w-full text-center
